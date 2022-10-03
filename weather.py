5bff07e789d48508ccdb87e417bbd37e799dbf71
FriskIsGit <friskisgit@users.noreply.github.com>
2022-10-03 17:45:53 +0200

    added new api, forced UTF8 to display city names properly

--- weather.py
+++ weather.py
@@ -2,12 +2,18 @@ import json
 from typing import Tuple
 import httpx
 
+FORCE_UTF8 = True
+
 #to grab geocoordinates
 OPENWEATHERMAP_API_KEY = 'e762d6c90e1f1670093b64960d9c7463'
+TOMORROW_API_KEY = 'ax2VNNEZxM0YM07kqGQS4QAdu5nReQ33'
 WEATHER_API_KEY = '97e5c007431143b496101324220110'
 WEATHERBIT_API_KEY = 'ce4866e8bd9e4db3b43b542ee573cc2d'
 LONGEST_NAME_LEN = 58
 def main():
+    if FORCE_UTF8:
+        import sys
+        sys.stdout.reconfigure(encoding='utf-8')
     city_name = get_city()
     #city_name = 'London'
 
@@ -15,6 +21,8 @@ def main():
     call_weatherbit_API(city_name)
     print("---> weatherapi:")
     call_weather_API(city_name)
+    print("---> tomorrowapi")
+    call_tomorrow_API(city_name)
 
 def get_city():
     city_name = input("Enter city name: ")
@@ -38,12 +46,39 @@ def get_geocoordinates_of(city_name) -> Tuple[int,int]:
 
         city_list = json.loads(response.text)
         cities = len(city_list)
-        print('Candidate cities:', cities)
+        #print('Candidate cities:', cities)
         if cities < 1:
             return 0,0
 
         first_city = city_list[0]
         return first_city['lat'], first_city['lon']
+
+def call_tomorrow_API(city_name):
+    lat, lon = get_geocoordinates_of(city_name)
+    if lat == 0 and lon == 0:
+        return
+    print("lat {} lon {}".format(lat,lon))
+    with httpx.Client() as client:
+        params = {
+            'location':'{},{}'.format(lat, lon),
+            'apikey':TOMORROW_API_KEY,
+            'fields':'temperature,temperatureApparent,humidity,windSpeed,windGust,rainIntensity,cloudBase,cloudCover,visibility,weatherCode',
+            'timesteps':'current',
+            }
+        url = 'https://api.tomorrow.io/v4/timelines'
+        response = client.get(url, params=params)
+        status = response.status_code
+        if status != 200:
+            print("Status code:", status)
+            return
+        data_map = json.loads(response.text)['data']['timelines'][0]
+        values = data_map['intervals'][0]['values']
+        print("Temperature {} C feels like {} C".format(values['temperature'], values['temperatureApparent']))
+        print("Wind speeds: {} kph with gusts up to {} kph".format(values['windSpeed'], values['windGust']))
+        base = values['cloudBase']
+        if base is None:
+            base = float('NaN')
+        print("Cloud coverage: {}% with cloud base at {} m".format(values['cloudCover'], base*1000))
 
 def call_weatherbit_API(city_name):
     with httpx.Client() as client:
@@ -60,13 +95,10 @@ def call_weatherbit_API(city_name):
         json_load = json.loads(response.text)
         if 'data' in json_load:
             data_map = json_load['data'][0]
-            try:
-                print('{} {}   Last observation time: {}'.format(data_map['city_name'], data_map['country_code'], data_map['ob_time']))
-            except UnicodeEncodeError as exc:
-                print('{}   Last observation time: {}'.format(data_map['country_code'], data_map['ob_time']))
+            print('{} {}   Last observation time: {}'.format(data_map['city_name'], data_map['country_code'], data_map['ob_time']))
             print('Temperature: %.1f C'%data_map['temp'])
             print('Sunrise: %s  Sunset: %s'%(data_map['sunrise'], data_map['sunset']))
-            print('Description:', data_map['weather']['description'],'  Cloud Coverage', data_map['clouds'], "%")
+            print('Description: {}      Cloud Coverage {}%'.format(data_map['weather']['description'],data_map['clouds']))
             print('Snow:', data_map['snow'])
 
 def call_weather_API(city_name):
